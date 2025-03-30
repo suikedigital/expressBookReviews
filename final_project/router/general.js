5fa72a42a3539cf558d4c308baf031148a897c3a
suikedigital <fraser.higginson@gmail.com>
2025-03-30 03:31:13 -0400

    brought repop back up to date with yesterdays code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,17 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({ books: books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn])
+  } else {
+        return res.status(404).json({message: "book not found"})
+  }
  });
   
 // Get book details based on author
